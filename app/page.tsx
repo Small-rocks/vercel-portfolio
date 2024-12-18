40e8b3b635017b68e7604d92ee20d9b72f216cdb
Big-Rock <riken9451@gmail.com>
2024-12-18 11:03:54 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import "./page.css"
+
+export default function Home() {
+  return (
+    <div>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="navbar">
+          <div>
+            <h1 className="width-10px height-20px weight-100px">
+              Farrukh Isakov
+            </h1></div>
+        </div>
+        <div className="supa-body">
+          <h2>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit repellat voluptas, sapiente maxime sit, distinctio expedita ad porro in atque necessitatibus et. Quam asperiores voluptates vitae. Omnis architecto magnam cumque, itaque ad laudantium dolore sit fugiat minima modi soluta ut eius deserunt laboriosam quod, labore eos eaque! Eius illo exercitationem quisquam omnis ad! Laudantium voluptatibus ex sequi? Impedit accusantium nulla perspiciatis consequuntur ratione repudiandae sint, tenetur officiis vero dignissimos explicabo voluptate, provident molestias! Dolor sapiente nam sunt quos, voluptate, adipisci eum, neque expedita ea magni nulla reiciendis alias veniam dolores! Quas quidem accusantium laborum aut facere aliquam deleniti nisi nulla ea possimus dolor, architecto magnam, sunt officiis culpa, rem molestias nesciunt eum debitis. Aliquid, dolorem. Deleniti neque totam eaque, dignissimos ratione vitae ipsum quod iure? Quo amet inventore praesentium facilis architecto alias blanditiis necessitatibus excepturi at, saepe in. Distinctio eius ducimus, ipsam nisi commodi quod deleniti voluptas ullam maiores eos cum adipisci exercitationem rerum quia asperiores illo earum reprehenderit. Iure laboriosam optio cupiditate dolorum dicta unde veniam id velit voluptas sit! Similique vel aspernatur esse sunt distinctio quas porro! Consequatur soluta tempora sint porro ullam perferendis, laboriosam, vero repudiandae, nobis illo quidem minus exercitationem magni adipisci iusto excepturi fuga doloribus. Qui ex ea quo provident atque cum minima odio, dicta autem aliquam veniam quos? Eligendi doloremque, eos totam temporibus, beatae sunt molestias maxime reiciendis quo quasi sed recusandae illum omnis. Et voluptas neque veritatis iusto! Repudiandae eveniet, repellendus eum quod sed aperiam laborum optio saepe possimus placeat modi commodi eaque. Ullam architecto ab ipsa praesentium a, sit tempora repellendus vero eum eius voluptas expedita velit quod maiores minima ad recusandae adipisci placeat pariatur corporis facere aliquid perferendis beatae? Quaerat modi dicta, ratione mollitia quo, sequi nulla aut porro earum aliquam minus repellendus animi error cumque numquam provident tenetur quia laudantium?
+          </h2>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://t.me/King_of_Nothing1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ask something
+        </a>
+      </footer>
+    </div>
+  );
+}
